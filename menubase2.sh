9c06d78bfc428925a677aec593ee65931208bcd0
NEBULA1978 <102324836+NEBULA1978@users.noreply.github.com>
2022-12-14 04:30:27 +0100

    Add files via upload
    Menu while y case con otros 3 submenus con bash.
    chmod +x menubase.sh
    ,/menubase.sh

---
+++ menubase2.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+while :; do
+    #Menu
+    clear
+    echo -e "\e[1;32m"
+    echo " MENU SCRIPT V.2 "
+    echo ""
+    echo "1. Mostrar directorio"
+    echo "2. Mostrar calendario"
+    echo "3. Mostrar dato"
+    echo "5.Ejecutar menubase.sh"
+    echo "6.Ejecutar menubase3.sh"
+    echo "7.Ejecutar menubase4.sh"
+    echo "4. Salir"
+    echo ""
+    #Escoger menu
+    echo -n "Escoger opcion: "
+    read opcion
+    #Seleccion de menu
+    case $opcion in
+    1)
+        echo "Mostrando directorio"
+        ls
+        read foo
+        ;;
+    2)
+        echo "Mostrando calendario"
+        cal
+        read foo
+        ;;
+    3)
+        echo "Mostrando datos"
+        date
+        read foo
+        ;;
+
+    # echo "5.Ejecutar menubase1.sh"
+    5)
+        echo "Ejecutar menubase.sh"
+        ./menubase.sh
+        read foo
+        ;;
+
+    # echo "6.Ejecutar menubase3.sh"
+    6)
+        echo "Ejecutar menubase3.sh"
+        ./menubase3.sh
+        read foo
+        ;;
+
+    # echo "7.Ejecutar menubase4.sh"
+    7)
+        echo "Ejecutar menubase4.sh"
+        ./menubase4.sh
+        read foo
+        ;;
+
+    4) exit 0 ;;
+    #Alerta
+    *)
+        echo "Opcion invalida..."
+        sleep 1
+        ;;
+    esac
+done
